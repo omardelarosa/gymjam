1d665539199629dd85779f6512ea3068f0d43ddc
Omar Delarosa <thedelarosa@gmail.com>
2020-04-17 14:47:48 -0400

    changing to scratch dir

--- runscript_me_100000.sh
+++ runscript_me_100000.sh
@@ -11,13 +11,19 @@
 /bin/hostname
 /bin/pwd
 
-module load python3/intel/3.6.3
+#module load python3/intel/3.6.3
+
+eval "$(pyenv init -)"
+pyenv activate lunarlander
+
+# Where results are going to be written
+OUTDIR=/scratch/od356/me_100k
 
 for n in {1..20}
 do
     python lunarlandercolab.py \
            --search-type ME \
            --num-individuals 100000 \
-           --checkpoint-dir checkpoints/test \
+           --checkpoint-dir $OUTDIR \
            --checkpoint-enabled
 done
